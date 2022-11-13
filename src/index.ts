4c12b1f7e993ca7fbfd528fb351f237aab6bdd86
Bartek O <bartek@fos.nl>
2022-11-13 21:10:52 +0100

    Made the transformer functional
    The transformer now does the following:
    1. go through all nodes to find a switch statement
    2. go through all clauses in the switch statement
       (to check if each has a break statement)
    3. combine all the clauses into if else statements
    4. add a `//switch` comment in front of the now if switch statement

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,124 @@ import {} from "ts-expose-internals";
 /**
  * Transformer entry point
  */
-export default function<T extends ts.Node>(program: ts.Program): ts.TransformerFactory<T>
-{
-	return (context) => {
-		const visit: ts.Visitor = node => {
-			if (ts.isSwitchStatement(node)) {
-				return [
-					// ts.factory.createJSDocText("wowie"),
-					node
-				]
+const transformer: ts.TransformerFactory<ts.Node> = context => {
+	const visit: ts.Visitor = node => {
+
+		if (ts.isSwitchStatement(node)) {
+			node = parseSwitchStatement(context)(node);
+		}
+
+		return ts.visitEachChild(node, child => visit(child), context);
+	};
+
+	return (node => ts.visitNode(node, visit)) as ts.Transformer<ts.Node>;
+}
+
+
+const parseSwitchStatement: ts.TransformerFactory<ts.SwitchStatement> = context => {
+	const visit: ts.Visitor = switchStatement => {
+
+		if (!ts.isSwitchStatement(switchStatement)) {
+			throw new Error("Called parseSwitchStatement on a none-switchStatement node");
+		}
+
+
+		const clauses: {case: ts.CaseClause, content: ts.Statement[]}[] = [];
+		let defaultClause: {case: ts.DefaultClause, content: ts.Statement[]} | undefined;
+
+		let everyCaseClauseHasABreak = true;
+		// go through every case to see if they all use a break statement
+		ts.visitEachChild(switchStatement.caseBlock, caseBlock => {
+
+			if (!(ts.isCaseClause(caseBlock) || ts.isDefaultClause(caseBlock))) {
+				throw new Error(`Child of CaseBlock is neither a CaseClause nor a DefaultClause`);
 			}
 
-			return ts.visitEachChild(node, child => visit(child), context);
-		};
+			let caseBlockHasABreak = false;
+			const currentBlockContent: ts.Statement[] = [];
 
-		return node => ts.visitNode(node, visit);
+			// go through every node in this case, to see if one is a break statement
+			caseBlock.statements.forEach(node => {
+
+				if (ts.isBreakStatement(node)) {
+					caseBlockHasABreak = true;
+
+					// only add nodes before the first break to the list (below)
+					return node;
+				}
+
+				if (ts.isStatement(node)) {
+					currentBlockContent.push(node);
+				} else {
+					throw new Error("Child of a CaseClause/DefaultClause is not a statement");
+				}
+
+				return node;
+			}, context);
+
+			// if no break was found, every becomes false
+			// if it's already false it won't change
+			everyCaseClauseHasABreak &&= caseBlockHasABreak;
+
+			if (everyCaseClauseHasABreak) {
+				if (ts.isCaseClause(caseBlock)) {
+					clauses.push({
+						case: caseBlock,
+						content: currentBlockContent
+					})
+				} else {
+					defaultClause = {
+						case: caseBlock,
+						content: currentBlockContent
+					}
+				}
+			}
+
+			return caseBlock;
+		}, context);
+
+
+		if (!everyCaseClauseHasABreak) {
+			// keep going like normal
+			return ts.visitEachChild(switchStatement, transformer(context), context);
+		}
+
+		// topBlock is default content, or last clause content
+		let topNode: ts.Block | ts.IfStatement = ts.factory.createBlock(defaultClause?.content ?? [], true);
+
+		clauses.forEach(clauseHolder => {
+
+			let nextNode = ts.factory.createIfStatement(
+				ts.factory.createBinaryExpression(
+					switchStatement.expression,
+					ts.factory.createToken(ts.SyntaxKind.EqualsEqualsEqualsToken),
+					clauseHolder.case.expression
+				),
+
+				ts.factory.createBlock(clauseHolder.content),
+
+				topNode
+			);
+
+			topNode = nextNode;
+
+		});
+
+
+		// add 'switch' comment
+		ts.addSyntheticLeadingComment(
+			topNode,
+			ts.SyntaxKind.SingleLineCommentTrivia,
+			"switch",
+			true
+		);
+
+		// go deeper like normal
+		return transformer(context)(topNode);
 	}
+
+	return (node => ts.visitNode(node, visit)) as ts.Transformer<ts.SwitchStatement>;
 }
+
+
+export default transformer;
