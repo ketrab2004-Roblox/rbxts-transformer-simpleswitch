ca60dbc99f99141cb9c3d319242de65d4ee1d893
Bartek O <bartek@fos.nl>
2022-11-13 20:55:11 +0100

    Replaced `ts-transformer-testing-library` with ts calls
    using `ts.createSourceFile()` to compile/read to ast
    and `ts.createPrinter()` to get it as text for testing
    
    also removed the library from `package.json` and `index.test.ts`

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,24 @@
-import { Transformer } from "ts-transformer-testing-library";
+import * as ts from "typescript";
 import switchTransformer from "./index";
+
+
+const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed,
+    removeComments: false
+});
+function applyTransformer(code: string): string {
+    const sourceFile = ts.createSourceFile(
+        "index.ts",
+        code,
+        ts.ScriptTarget.ES2015,
+        true,
+        ts.ScriptKind.TS
+    );
+
+    const result = switchTransformer(ts.nullTransformationContext)(sourceFile);
+
+    return printer.printNode(ts.EmitHint.SourceFile, result, sourceFile);
+}
 
 
 describe("Testing the switch case transformer", () => {
@@ -7,33 +26,8 @@ describe("Testing the switch case transformer", () => {
         expect(true).toBeTruthy();
     });
 
-    const transformersSetup = new Transformer()
-        .addTransformer(switchTransformer);
-
     test("An actual test", () => {
-        // const result = transformString(`
-        // let a = 2;
-
-        // switch (a) {
-        //     case 1:
-        //         console.log("one");
-        //         break;
-
-        //     case 2:
-        //         console.log("two");
-        //         break;
-
-        //     default:
-        //         console.log("uhmmm???");
-        //         break;
-        // };
-        // `, {
-        //     transforms: [
-        //         switchTransformer
-        //     ]
-        // });
-
-        const result = transformersSetup.transform(`
+        const result = applyTransformer(`
         let a = 2;
 
         switch (a) {
@@ -55,6 +49,7 @@ describe("Testing the switch case transformer", () => {
 
         expect(result).toBe(`
         let a = 2
+        // switch
         if (a == 1) {
             console.log("one");
         } elseif (a == 2) {
